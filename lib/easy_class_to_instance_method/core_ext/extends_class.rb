982bd61d0af4be771263e9c447092e573eb13000
Ramon Tayag <ramon.tayag@gmail.com>
2013-07-11 12:11:47 +0800

    Remove activesupport requirement

--- lib/easy_class_to_instance_method/core_ext/extends_class.rb
+++ lib/easy_class_to_instance_method/core_ext/extends_class.rb
@@ -2,11 +2,11 @@ module EasyClassToInstanceMethod
   module CoreExt
     module ExtendsClass
 
-      extend ActiveSupport::Concern
-
-      included do
-        def easy_class_to_instance
-          include EasyClassToInstanceMethod
+      def self.included(base)
+        base.class_eval do
+          def easy_class_to_instance
+            include EasyClassToInstanceMethod
+          end
         end
       end
 
